fb053f637fbecca41e0da6ea63c78a663991beb1
chfw <wangc_2011@hotmail.com>
2018-09-08 08:02:19 +0100

    :sparkles: support subcliping

--- youtube2mp3/main.py
+++ youtube2mp3/main.py
@@ -1,28 +1,40 @@
 import os
+import sys
 
-import fire
+from urllib.parse import urlparse
 
 from youtube2mp3.audio_dl import YoutubeAudioDL
 from youtube2mp3.mp3 import save_as_mp3
 
 
-def batch_download(*urls):
-    with YoutubeAudioDL() as dl:
-        dl.download(urls)
-        for file_name in dl.file_names:
-            save_as_mp3(file_name)
-            os.unlink(file_name)
-
-
-def get_clip(url, start, stop):
-    pass
+URL = "https://www.youtube.com/watch?v="
 
 
 def main():
-    fire.fire({
-        'batch': batch_download,
-        'clip': get_clip
-        })
+    urls = sys.argv[1:]
+    with YoutubeAudioDL() as dl:
+        clip_info_list = []
+        cleansed_url = []
+        for url in urls:
+            result = urlparse(url)
+            query, number = get_query(result.query)
+            cleansed_url.append(URL + query['v'])
+            clip_info_list.append(query)
+        dl.download(urls)
+        for file_name, clip_info in zip(dl.file_names, clip_info_list):
+            save_as_mp3(file_name, clip_info)
+            os.unlink(file_name)
+
+
+def get_query(query):
+    tokens = query.split('&')
+    abc = {}
+    number_of_keys = 0
+    for token in tokens:
+        key, value = token.split('=')
+        abc[key] = value
+        number_of_keys += 1
+    return abc, number_of_keys
 
 
 if __name__ == "__main__":
--- youtube2mp3/mp3.py
+++ youtube2mp3/mp3.py
@@ -3,7 +3,12 @@ import os
 from moviepy.editor import AudioFileClip
 
 
-def save_as_mp3(file_name):
+def save_as_mp3(file_name, clip_info):
     clip = AudioFileClip(file_name)
+    if 'start' in clip_info or 'stop' in clip_info:
+        start = clip_info.get('start', 0)
+        stop = clip_info.get('stop', None)
+        import pdb; pdb.set_trace()
+        clip = clip.subclip(start, stop)
     name, suffix = os.path.splitext(file_name)
     clip.write_audiofile('{}.mp3'.format(name))
